142fac96f7338b7d243e75500fa26344eeddc159
admalungo <52615957+admalungo@users.noreply.github.com>
2026-07-06 07:10:44 -0400

    Added content to /categories page(s)

---
+++ app/category/page.tsx
@@ -0,0 +1,8 @@
+export default function CategoryDetails() {
+    return (
+        <main>
+            <h1>Category Details Page</h1>
+            <p>Display all entries in the categories. It may not even be necessary to display on a separate page. May just use columns instead on the previous page.</p>
+        </main>
+    )
+}
